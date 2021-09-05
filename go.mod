d5ae4ab53417cef0a359c3826e82eff5b27d939d
Raunow <steff.bpoulsen@gmail.com>
2021-09-05 16:56:35 +0200

    prep

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/raunow/justle
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	// "flag"
+	"fmt"
+	// "net/http"
+	// "io/ioutil"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+}
